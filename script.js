0adc1f4cd7794175070aba406775b3c2561cb239
J0nWater <79053067+J0nWater@users.noreply.github.com>
2024-09-19 14:22:52 +0200

    Initi

---
+++ script.js
@@ -0,0 +1,62 @@
+let points = 0;
+let number1 = 0;
+let number2 = 0;
+let userInput = '';
+let endGameText = '';
+
+updateWiev()
+function updateWiev(){
+    document.getElementById('app').innerHTML = /*HTML*/`
+    <h1>Krokodillespill<h1><hr>
+    <div>Points: ${points}</div>
+    <img src="crocodile.png">
+
+    <div class="gameStyle">
+        <div>${number1}</div>
+        <input onchange="setSign(this.value)">
+        <div>${number2}</div>
+    </div>
+    <button onclick="checkInput()">Submit</button>
+    <button onclick="randomNumber()">Reset</button>
+    <div class="endText">${endGameText}</div>
+    `
+}
+
+function randomNumber(){
+    number1 = getRandomNumber()
+    number2 = getRandomNumber()
+    
+    updateWiev()
+}
+
+function getRandomNumber(){
+    return Math.floor(Math.random()*11);
+}
+
+function checkInput(){
+    if(number1 < number2 && userInput == '<' || number1 > number2 && userInput == '>'
+        || number1 == number2 && userInput == '=')
+        {
+        points++
+        console.log("riktig")
+    }
+    else{
+        points--
+        console.log("feil")
+    }
+    checkPoints()    
+    updateWiev()
+}
+
+function setSign(sign){
+    userInput = sign;
+}
+
+function checkPoints(){
+    if(points >= 5){
+        endGameText = "You Won!!!🥇";
+    }
+    else if(points <= -5){
+        endGameText = "You Lost😭";
+    }
+}
